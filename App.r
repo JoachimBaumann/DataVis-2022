adbfbe2627228225406a07b3f4be20fe1c20f6ed
MizzleTheHorse <emil.madsen@live.dk>
2022-10-21 13:22:16 +0200

    Filtering and extraction of data from csv file. Plotting coordinates on map. Plotting states and shapes. Uploaded to git

--- App.r
+++ App.r
@@ -5,10 +5,40 @@
 #Source: https://data.world/aarranzlopez/ufo-sights-2016-us-and-canada
 
 library(xlsx)
+library(tidyverse)
+library(sf)
+library(mapview)
+library(viridis)
+library(RColorBrewer)
 
-ourdata <- read.xlsx("C:/Users/Emil/Desktop/UNI/DataVisualization/R_opgaver/Project/UFOs_coord-1.xlsx", 1)
-
-summary(ourdata)
 
 
-plot(ourdata$lat, ourdata$lng, xlab = "Latitude", ylab = "Longitude")
+ourdata <- read.xlsx("C:/Users/Emil/Desktop/UNI/DataVisualization/R_opgaver/Project/DataVis-2022/UFOs_coord-1.xlsx", 1)
+
+#summary(ourdata)
+ourdata %>% 
+  glimpse()
+
+
+##Filter to get only state with California. 
+#ourdata <- ourdata  %>% 
+#  filter(State == "CA")
+
+#plot(ourdata$lat, ourdata$lng, xlab = "Latitude", ylab = "Longitude")
+
+
+##This is to visualize the different locations on a map. 
+mapview(ourdata, xcol = "lng", ycol = "lat", crs = 4269, grid = FALSE)
+
+colfunc <- colorRampPalette(c("white", "black"))
+
+counts_shape <- table(ourdata$Shape)
+barplot_shapes <- barplot(counts_shape, main="Shape distribution",
+        xlab="Shapes observed", col=colfunc(30) , beside=False)
+
+
+counts_state <- table(ourdata$State)
+barplot_shapes <- barplot(counts_state, main="State distribution",
+                          xlab="Observations in states", col=colfunc(60) , beside=False)
+
+

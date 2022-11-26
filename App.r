703dde5704e19accad3fecfb904fe76010395c92
MizzleTheHorse <emil.madsen@live.dk>
2022-11-26 15:39:36 +0100

    Started refactoring wordcloud, added wordcloud function

--- App.r
+++ App.r
@@ -68,25 +68,45 @@ p <- ggplot(
 
 ####Wordcloud
 
-text <- readLines("./words.txt")
+getWordCloud <- memoise(function() {
+  # Careful not to let just any name slip in here; a
+  # malicious user could manipulate this value.
+  
+  text <- readLines(sprintf("./%s.txt.gz", book),
+                    encoding="UTF-8")
+  
+  myCorpus = Corpus(VectorSource(text))
+  myCorpus = tm_map(myCorpus, content_transformer(tolower))
+  myCorpus = tm_map(myCorpus, removeWords,
+                    c(stopwords("SMART"), "thy", "thou", "thee", "the", "and", "but"))
+  
+  myDTM = TermDocumentMatrix(myCorpus,
+                             control = list(minWordLength = 1))
+  
+  m = as.matrix(myDTM)
+  
+  sort(rowSums(m), decreasing = TRUE)
+  
+})
 
-myCorpus = Corpus(VectorSource(text))
-myCorpus = tm_map(myCorpus, content_transformer(tolower))
-#myCorpus = tm_map(myCorpus, removePunctuation)
-#myCorpus = tm_map(myCorpus, removeNumbers)
-#myCorpus = tm_map(myCorpus, removeWords,
-#                  c(stopwords("SMART"), "thy", "thou", "thee", "the", "and", "but"))
 
-myDTM = TermDocumentMatrix(myCorpus,
-                          control = list(minWordLength = 1))
 
-m = as.matrix(myDTM)
+###
+#text <- readLines("./words.txt")
+#
+#myCorpus = Corpus(VectorSource(text))
+#myCorpus = tm_map(myCorpus, content_transformer(tolower))
 
-sort(rowSums(m), decreasing = TRUE)
+#myDTM = TermDocumentMatrix(myCorpus,
+#                          control = list(minWordLength = 1))
+
+#m = as.matrix(myDTM)
 
 #sort(rowSums(m), decreasing = TRUE)
 
-wordcloud(text)
+#sort(rowSums(m), decreasing = TRUE)
+
+#wordcloud(text)
 ###
 
 
